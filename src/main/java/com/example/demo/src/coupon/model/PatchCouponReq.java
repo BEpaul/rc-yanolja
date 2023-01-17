c77acef8c9198339166e6afb00133f6a9c378609
Minhyuk Kim <rhrlwld@gmail.com>
2023-01-18 03:27:52 +0900

    feat: add feature when using coupon

---
+++ src/main/java/com/example/demo/src/coupon/model/PatchCouponReq.java
@@ -0,0 +1,15 @@
+package com.example.demo.src.coupon.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatchCouponReq {
+    private Long couponId;
+    private Long status;
+}
